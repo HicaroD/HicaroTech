0f7479d958fefcd8227343f285384e8784b7ffd6
HicaroD <hdanrlley1@gmail.com>
2022-04-09 01:46:19 -0300

    Fix path to static files

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [BASE_DIR / "articles/static"]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "articles/static/css/"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
