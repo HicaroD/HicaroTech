3e19fba0070ef4c16afba63e6f7f7d16cb6fc85a
HicaroD <hdanrlley1@gmail.com>
2022-04-09 00:31:33 -0300

    Simple homepage view (It will change)

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def homepage(request):
-    return "<h1>Hello, world!</h1>"
+    return HttpResponse("<h1>Hello, world!")
