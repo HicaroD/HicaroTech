cab64e29b567efb21bbc4b7ffb6505714d2b0f24
HicaroD <hdanrlley1@gmail.com>
2022-04-09 00:30:40 -0300

    Building `Article` model

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    name = models.CharField(max_length=50)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-created_at"]
