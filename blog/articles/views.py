d89ac4c70b2b142067cf885ba7d1db8f0db3eb42
HicaroD <hdanrlley1@gmail.com>
2022-04-09 01:14:20 -0300

    Rendering `homepage.html` template (not implemented)

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse("<h1>Hello, world!")
+    return render(request, 'homepage.html')
