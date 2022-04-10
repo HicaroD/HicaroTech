c6193fa1fe124ea63740d8644f15e7f0ea77e243
HicaroD <hdanrlley1@gmail.com>
2022-04-10 15:34:42 -0300

    [Backend] Adding `DetailView` url with `slug`

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name="homepage"),
+    path('<slug:slug>', views.ArticleDetailView.as_view(), name="article_detail")
 ]
