e6bb59053edcf932efd95498aa2c6310205a1db1
HicaroD <hdanrlley1@gmail.com>
2022-04-12 14:35:21 -0300

    [Backend] Fix problem with `null` on database

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -7,7 +7,7 @@ class Article(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, null=True)
 
     def __str__(self):
         return self.name
